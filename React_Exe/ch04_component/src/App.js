2223127df717651cff4272628cc426fe5a913445
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-17 12:49:13 +0900

    ch04 생성

---
+++ React_Exe/ch04_component/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+// import Child from './ex01_function/Child';
+import Child from './ex02_class/Child';
+
+function App() {
+  return <Child />;
+}
+
+export default App;
