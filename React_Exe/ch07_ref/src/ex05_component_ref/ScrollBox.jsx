6fb884394ff767f79a442e7828b19fea92900fd8
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-20 16:24:28 +0900

    주석, 코드 보충

--- React_Exe/ch07_ref/src/ex05_component_ref/ScrollBox.jsx
+++ React_Exe/ch07_ref/src/ex05_component_ref/ScrollBox.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 
 class ScrollBox extends Component {
+    // 스크롤 바를 가장 아래로 내리도록 위치
+    scrollToBottom = () => {
+        // scrollTop: 세로 스크롤바 위치
+        // scrollHeight: 스크롤이 있는 박스 안의 div 높이
+        // clientHeight: 스크롤리 있는 박스의 높이
+        const {scrollHeight, clientHeight} = this.box;
+        this.box.scrollTop = scrollHeight - clientHeight;
+    };
+
     render() {
       const style = {
         border: '1px solid black',
@@ -22,6 +31,16 @@ class ScrollBox extends Component {
           ref={(ref) => {this.box=ref}}>
           <div style={innerStyle}/>
         </div>
+        
+        // 2안
+        // <div
+        // style={style}
+        // ref={(ref) => {
+        //   this.box = ref;
+        // }}
+        // >
+        //     <div style={innerStyle} />
+        // </div>
       );
     }
   }
