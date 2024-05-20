e320846df30ee801be973041cd7774cda59b0139
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-20 16:44:35 +0900

    컴포넌트 반복을 통한 데이터 제거 기능 구현

--- React_Exe/ch08-array_component/src/App.js
+++ React_Exe/ch08-array_component/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 //import IterationSample from './ex01_map/IterationSample';
 // import IterationSample from './ex02_key/IterationSample';
 // import IterationSample from './ex03_id/IterationSample';
-import IterationSample from './ex04_add_name/IterationSample';
+// import IterationSample from './ex04_add_name/IterationSample';
+import IterationSample from './ex05_remove_name/IterationSample';
 
 function App() {
   return <IterationSample />
