38fb01749fdd1e0ec300b6e1eba0cb9ec94f4e61
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-20 13:24:38 +0900

    함수형 컴포넌트로 이벤트 핸들링 구현

--- React_Exe/ch06_event/src/App.js
+++ React_Exe/ch06_event/src/App.js
@@ -6,8 +6,11 @@ import './App.css';
 // import EventPractice from './ex04_onChange_state/EventPractice';
 // import EventPractice from './ex05_function_bind/EventPractice';
 // import EventPractice from './ex06_function_bind/EventPractice';
-import EventPractice from './ex07_multi_setstate/EventPractice';
+// import EventPractice from './ex07_multi_setstate/EventPractice';
+// import EventPractice from './ex08_onKeyPress/EventPractice';
+// import EventPractice from './ex09_function_event/EventPractice';
 
+import EventPractice from './ex10_function_multi_setstate/EventPractice';
 function App() {
   return  <EventPractice />;
 }
