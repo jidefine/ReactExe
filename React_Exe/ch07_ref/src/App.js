51ee3499057aa57cf6107799f14962abf9492f22
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-20 15:47:32 +0900

    컴포넌트에 ref 달기

---
+++ React_Exe/ch07_ref/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+// import ValidationSample from './ex02_validation/ValidationSample';
+// import ValidationSample from './ex03_validation_ref/ValidationSample';
+// import ValidationSample from './ex04_validation_createRef/ValidationSample';
+
+import React, { Component } from 'react';
+import ScrollBox from './ex05_component_ref/ScrollBox';
+
+// function App() {
+//   return <ValidationSample />
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox/>
+      </div>
+    );
+  }
+}
+
+export default App;
