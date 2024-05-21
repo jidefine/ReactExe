c50530f53948b91cd7426f0f5db2a841f50fc464
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-21 16:27:47 +0900

    기본 html

---
+++ React_Exe/ch11_crud/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+// import SubApp from './ex01_full_html/SubApp';
+// import SubApp from './ex02_divide_html/SubApp';
+// import SubApp from './ex03_divide_html/SubApp';
+// import SubApp from './ex04_props/SubApp';
+// import SubApp from './ex05_list/SubApp';
+// import SubApp from './ex06_nav_list/SubApp';
+// import SubApp from './ex07_nav_list/SubApp';
+// import SubApp from './ex08_nav_list/SubApp';
+
+import SubApp from './ex09_list_key/SubApp';
+function App() {
+  return <SubApp />;
+}
+
+export default App;
