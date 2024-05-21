73385c0ef03060a5426822db18b4ff09cb3fa98a
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-21 12:53:54 +0900

    주석 추가

--- React_Exe/ch10_hooks_tutorial/src/ex10_useReducer/Info.js
+++ React_Exe/ch10_hooks_tutorial/src/ex10_useReducer/Info.js
@@ -2,8 +2,8 @@ import React, { useReducer } from 'react';
 
 function reducer(state, action){
     return {
-        ...state,
-        [action.name]: action.value
+        ...state, // 요청대로 state를 덮어씀
+        [action.name]: action.value // e.target에 맞게 action 실행
     }
 }
 
@@ -14,7 +14,7 @@ const Info = () => {
     });
     const {name, nickname} = state;
     const onChange = e => {
-        dispatch(e.target);
+        dispatch(e.target); // action에게 전달
     };
 
     return (
