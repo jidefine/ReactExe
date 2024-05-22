7c13f261d120a10687648183ee2651fe6de75f8b
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-22 13:57:39 +0900

    DELETE 기능 완료

--- React_Exe/ch11_crud/src/App.js
+++ React_Exe/ch11_crud/src/App.js
@@ -30,10 +30,10 @@ import './App.css';
 // import SubApp from './ex28_update/SubApp';
 // import SubApp from './ex29_update/SubApp';
 // import SubApp from './ex30_update/SubApp';
-import SubApp from './ex31_update/SubApp';
+// import SubApp from './ex31_update/SubApp';
 
 // import SubApp from './ex32_delete/SubApp';
-// import SubApp from './ex33_delete/SubApp';
+import SubApp from './ex33_delete/SubApp';
 // import SubApp from './ex35_divide_file/SubApp';
 
 function App() {
