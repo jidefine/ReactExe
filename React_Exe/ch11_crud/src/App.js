cc6ec9a83ba49bd3c3420ae7d8b6a6a314ac19c4
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-22 13:55:39 +0900

    <Update/> 기능완료

--- React_Exe/ch11_crud/src/App.js
+++ React_Exe/ch11_crud/src/App.js
@@ -29,8 +29,8 @@ import './App.css';
 // import SubApp from './ex27_update/SubApp';
 // import SubApp from './ex28_update/SubApp';
 // import SubApp from './ex29_update/SubApp';
-import SubApp from './ex30_update/SubApp';
-// import SubApp from './ex31_update/SubApp';
+// import SubApp from './ex30_update/SubApp';
+import SubApp from './ex31_update/SubApp';
 
 // import SubApp from './ex32_delete/SubApp';
 // import SubApp from './ex33_delete/SubApp';
