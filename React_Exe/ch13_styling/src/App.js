9f5c7916161d63c1c60ae57350aab6019752be93
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-23 13:30:55 +0900

    Sass의 이해

---
+++ React_Exe/ch13_styling/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SassComponents from './ex01_sass/SassComponents';
+ 
+function App() {
+    return (
+      <div>
+        <SassComponents />
+      </div>
+    );
+}
+
+export default App;
+
