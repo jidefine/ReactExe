2a4766e613ebdde445a9501b23175c061c8c41c8
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-23 10:13:02 +0900

    Styled Button

---
+++ React_Exe/ch12_styled/src/ex02_styled/StyledButton.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+// css 디자인 설정명과 동일하게
+const StyledButton = styled.button `
+        color: white;
+        background-Color: purple;
+        margin: 10px;
+`;
+
+export default StyledButton;
