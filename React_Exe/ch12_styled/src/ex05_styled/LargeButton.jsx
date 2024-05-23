e328aa8d1e17327f274b4df7932c6fe035a61027
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-23 10:15:06 +0900

    Styled LargeButton

---
+++ React_Exe/ch12_styled/src/ex05_styled/LargeButton.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import SimpleButton from './SimpleButton';
+
+const LargeButton = styled(SimpleButton) `
+    font-size: 50px;
+    margin: 10px;
+`;
+
+export default LargeButton;
