bd1b68f2d178d6450890817792a051222d8033fd
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-24 14:55:13 +0900

    입력한 항목 TodoListItem으로 생성

--- React_Exe/ch14_todoapp/src/App.js
+++ React_Exe/ch14_todoapp/src/App.js
@@ -1,13 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 // import TodoTemplate from './ex01_TodoTemplate/TodoTemplate';
-import TodoTemplate from './ex02_TodoInsert/TodoTemplate';
-import TodoInsert from './ex02_TodoInsert/TodoInsert';
+// import TodoTemplate from './ex02_TodoInsert/TodoInsert';
+// import TodoInsert from './ex02_TodoInsert/TodoInsert';
+
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './ex05_TodoList/TodoTemplate';
+import TodoInsert from './ex05_TodoList/TodoInsert'
+import TodoList from './ex05_TodoList/TodoList';
 
 function App() {
-  return <TodoTemplate>
-            <TodoInsert />
-          </TodoTemplate>
-}
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos}/>
+    </TodoTemplate>
+  );
+};
 
 export default App;
--- React_Exe/ch14_todoapp/src/ex03_TodoList/TodoInsert.jsx
+++ React_Exe/ch14_todoapp/src/ex03_TodoList/TodoInsert.jsx
@@ -1,10 +1,30 @@
 import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
+import { useCallback, useState } from 'react';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+
+            e.preventDefault();
+        },
+        [onInsert, value]
+    );
+
     return (
-        <from className='TodoInsert'>
-            <input placeholder='할 일 입력' />
+        <from className='TodoInsert' onSubmit={onSubmit}>
+            <input placeholder='할 일 입력' 
+                value={value}
+                onChange={onChange}
+            />
             <button type='submit'>
                 <MdAdd />
             </button>
