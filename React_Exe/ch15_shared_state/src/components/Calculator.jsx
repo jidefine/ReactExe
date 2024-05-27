225e5634d3863ba523e6008d8cac2e0a72cdf81b
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-27 11:27:53 +0900

    Shared State 예제

---
+++ React_Exe/ch15_shared_state/src/components/Calculator.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import TemperatureInput from "./TemperatureInput";
+
+function Calculator(props) {
+
+    // 
+    const [temperature, setTemperature] = useState(''); // 온도
+    const [scale, setScale] = useState('c');            // 섭씨c 화씨f
+
+    const toCelsius = (fah) => ((fah - 32) * 5) / 9;    // ->
+    const toFahrenheit = (cel) => (cel * 9) / 5 + 32;   // ->
+
+    // temp 온도
+    // CONVERT 변환 함수
+    const tryConvert = (temp, convert) => {
+        // 실수로 변환
+        const input = parseFloat(temp);
+        if(Number.isNaN(input))
+            return "";
+        // 변환 작업
+        const output = convert(input);
+        // 반올림
+        const rounded = Math.round(output * 1000) / 1000;
+        //문자열로 반환
+        return rounded.toString();
+    }
+
+    // handleCelsiusChange로 넘어온 값을 매개변수를 temp로 받음
+    const handleCelsiusChange = (temp) => {
+        setTemperature(temp);
+        setScale("c");
+    }
+    const handleFahrenheitaChange = (temp) => {
+        setTemperature(temp);
+        setScale("f");
+    }
+
+    //scale은 현재 입력된 값이 f? c?
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+        <div>
+            <TemperatureInput 
+            scale="c"
+            temperature={celsius}
+            onTemperatureChange={handleCelsiusChange}/>
+            <TemperatureInput 
+            scale="f"
+            temperature={fahrenheit}
+            onTemperatureChange={handleFahrenheitaChange}/>
+        </div>
+    )
+} 
+
+export default Calculator;
---
+++ React_Exe/ch15_shared_state/src/components/TemperatureInput.jsx
@@ -0,0 +1,24 @@
+
+const scaleNames = {
+    c : '섭씨',
+    f: '화씨'
+};
+
+function TemperatureInput(props){
+
+    const {scale, temperature, onTemperatureChange} = props;
+
+    const handleChange = (event) => {
+        onTemperatureChange(event.target.value);
+    }
+    return (
+        <fieldset>
+            <legend>
+                온도 입력(단위: {scaleNames[props.scale]})
+            </legend>
+            <input value={props.temperature} onChange={handleChange} />
+        </fieldset>
+    )
+}
+
+export default TemperatureInput;
