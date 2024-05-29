ba461c30c48536764189b313123f9115364408c0
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-29 11:18:40 +0900

    동적으로 Route 구성하기(오류 수정 중)

---
+++ React_Exe/ch17_router/src/ex09_dynamic_nested/Contact.jsx
@@ -0,0 +1,11 @@
+
+function Contact(){
+    return (
+        <div>
+            <h2>Contact</h2>
+            Content...
+        </div>
+    )
+}
+
+export default Contact;
---
+++ React_Exe/ch17_router/src/ex09_dynamic_nested/Topic.jsx
@@ -0,0 +1,27 @@
+import { useParams } from "react-router-dom";
+
+function Topic(contents){
+    // useParams을 사용하면 현재 컴포넌트가 routing된 변수값을 얻을 수 있음
+    const params = useParams;
+    const topic_id = params.topic_id;
+    let selected_topic ={
+        title: 'Sorry',
+        description: 'Not Found'
+    };
+    for(let i=0; i<contents.length; i++){
+        if(contents[i].id === Number(topic_id)){
+            selected_topic = contents[i];
+            break;
+        }
+    }
+    console.log(params);
+
+    return (
+        <div>
+            <h3>{selected_topic.title}</h3>
+            {selected_topic.description}
+        </div>
+    );
+}
+
+export default Topic;
