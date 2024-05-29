eb7ca4999e1eb652fb02b77b8b030ac6840ba4f1
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-29 10:57:54 +0900

    Nested Routing

--- React_Exe/ch17_router/src/App.js
+++ React_Exe/ch17_router/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import SubApp from './ex06_hash_router/SubApp';
+import SubApp from './ex08_nested_routing/SubApp';
 
 function App() {
   return <SubApp />
