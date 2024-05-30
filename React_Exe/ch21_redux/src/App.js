509a93c430027e9a9e7fc02557d2d715e4caa8e7
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-30 15:20:32 +0900

    hierachy에 값 전달

--- React_Exe/ch21_redux/src/App.js
+++ React_Exe/ch21_redux/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import MainApp from './ex01_hierachy/MainApp';
-// import MainApp from './ex02_hierachy/MainApp';
+// import MainApp from './ex01_hierachy/MainApp';
+import MainApp from './ex02_hierachy/MainApp';
 // import MainApp from './ex03_divide/MainApp';
 
 function App() {
