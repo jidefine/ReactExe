d09fa2cbf64df038629a32a78fa1df0566acf4cc
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-30 15:37:31 +0900

    계층구조 파일 분할

---
+++ React_Exe/ch21_redux/src/ex03_divide/Left2.jsx
@@ -0,0 +1,12 @@
+import Left3 from "./Left3";
+
+function Left2(props) {
+    const {number} = props;
+    return (
+        <div>
+            <h1>Lef2: {number}</h1>
+            <Left3 number={number}/>
+        </div>
+    )
+}
+export default Left2;
