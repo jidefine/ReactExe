40067c68a84e5899a2aaa6c4bbabf728f2ae5489
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-30 13:51:50 +0900

    PostViewPage 컴포넌트 완료

--- React_Exe/ch18_mini_blog/src/App.js
+++ React_Exe/ch18_mini_blog/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 // import MiniBlogApp from './ex03_mini_blog_app/MiniBlogApp';
 // import MiniBlogApp from './ex04_main_page/MiniBlogApp';
-import MiniBlogApp from './ex05_post_wrtie_page/MiniBlogApp';
-// import MiniBlogApp from './ex06_post_view_page/page/MainPage';
+// import MiniBlogApp from './ex05_post_wrtie_page/MiniBlogApp';
+import MiniBlogApp from './ex06_post_view_page/MiniBlogApp';
+
 
 function App() {
-  return <MiniBlogApp />
+  return <MiniBlogApp />;
 }
 
 export default App;
--- React_Exe/ch18_mini_blog/src/ex05_post_wrtie_page/ui/Button.jsx
+++ React_Exe/ch18_mini_blog/src/ex05_post_wrtie_page/ui/Button.jsx
@@ -1,4 +1,4 @@
-import React from 'styled-components';
+import React from 'react';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
