545c649523b66c4d053ff5e9a876036c8bceb771
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-30 14:11:33 +0900

    useReducer의 사용법(오류 해결 중)

---
+++ React_Exe/ch20_use_reducer/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import MainApp from './ex01_state/MainApp';
+// import MainApp from './ex02_reducer/MainApp';
+// import MainApp from './ex03_change_number/MainApp';
+// import MainApp from './ex04_action/MainApp';
+
+function App() {
+  return <MainApp/>;
+}
+
+export default App;
