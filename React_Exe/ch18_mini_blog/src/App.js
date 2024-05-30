0583292296d26b3b704e86ceb69c060ab6877e6c
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-30 11:12:02 +0900

    PostWrtiePage 구현 중

--- React_Exe/ch18_mini_blog/src/App.js
+++ React_Exe/ch18_mini_blog/src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 // import MiniBlogApp from './ex03_mini_blog_app/MiniBlogApp';
 // import MiniBlogApp from './ex04_main_page/MiniBlogApp';
-import MiniBlogApp from './ex05_post_wrtie_page/MiniBlogApp';
-// import MiniBlogApp from './ex06_post_view_page/page/MainPage';
+// import MiniBlogApp from './ex05_post_wrtie_page/MiniBlogApp';
+import MiniBlogApp from './ex06_post_view_page/page/MainPage';
 
 function App() {
   return <MiniBlogApp />
