1aec8d63b5b2173b00fd470b38120ea8969447f3
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-30 15:47:57 +0900

    redux 사용 준비

--- React_Exe/ch21_redux/src/App.js
+++ React_Exe/ch21_redux/src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 // import MainApp from './ex01_hierachy/MainApp';
 // import MainApp from './ex02_hierachy/MainApp';
-import MainApp from './ex03_divide/MainApp';
+// import MainApp from './ex03_divide/MainApp';
+import MainApp from './ex04_redux/MainApp';
 
 function App() {
   return <MainApp/>
---
+++ React_Exe/ch21_redux/src/ex04_redux/Right3.jsx
@@ -0,0 +1,12 @@
+
+function Right3(props) {
+    const {onIncrease} = props;
+    return (
+        <div>
+            <h1>Right3 </h1>
+            <input type='button' value='+' />
+        </div>
+    )
+}
+
+export default Right3;
