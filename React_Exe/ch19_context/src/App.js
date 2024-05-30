c68e04eb93d78a5d88f8a936c10dbf6971b9b13d
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-30 11:44:00 +0900

    컴포넌트 분리

--- React_Exe/ch19_context/src/App.js
+++ React_Exe/ch19_context/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import MainApp from './ex01_hierachy/MainApp';
+// import MainApp from './ex01_hierachy/MainApp';
+import MainApp from './ex02_divide/MainApp';
 
 function App() {
   return <MainApp/>;
