35217537051fd4d8ce41c7868503ee2628fb3e5d
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-30 15:19:11 +0900

    hierachy 기본

---
+++ React_Exe/ch21_redux/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainApp from './ex01_hierachy/MainApp';
+// import MainApp from './ex02_hierachy/MainApp';
+// import MainApp from './ex03_divide/MainApp';
+
+function App() {
+  return <MainApp/>
+}
+
+export default App;
+
+// npm install --save redux react-redux
