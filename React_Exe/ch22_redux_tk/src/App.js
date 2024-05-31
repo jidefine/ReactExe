92f47ac112db4cf175690175c0c53ef5d57a3734
jidefine <148775766+jidefine@users.noreply.github.com>
2024-05-31 09:59:26 +0900

    redux 사용법

---
+++ React_Exe/ch22_redux_tk/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MainApp from './ex01_redux/MainApp';
+
+function App() {
+  return <MainApp/>;
+}
+
+export default App;
+
+// npm install --save redux react-redux @reduxjs/toolkit
