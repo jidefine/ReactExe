f12c3bd60956460ebe9c8bfaeadf0318403c1102
jidefine <148775766+jidefine@users.noreply.github.com>
2024-06-03 21:10:56 +0900

    JPA, 연관관계 복습(주석 추가)

--- CardDataBase/src/main/java/org/mind/cardatabase/domain/Car.java
+++ CardDataBase/src/main/java/org/mind/cardatabase/domain/Car.java
@@ -2,19 +2,19 @@ package org.mind.cardatabase.domain;
 
 import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.*; // Java Persistence API (JPA)에서 사용되는 주요 인터페이스와 클래스를 포함하는 패키지
 
 @Entity
 @Getter
 @Setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor // 모든 필드를 인자로 받는 생성자(Lombok의 어노테이션)
+@NoArgsConstructor  // 인자를 받지 않는 기본 생성자 생성(Lombok의 어노테이션)
 @ToString(exclude = "owner") // 필요할 때 따로 조회
 public class Car {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id // primary key = 엔티티의 식별자(identifier)
+    @GeneratedValue(strategy = GenerationType.AUTO) // JPA 엔터티의 주요 키(primary key) 값을 자동으로 생성
     private long id;
     private String brand;
     private String model;
@@ -24,7 +24,13 @@ public class Car {
     private int year;
     private int price;
 
+    // 지연 로딩(Lazy Loading) : 연관된 엔티티가 필요한 경우에만 데이터를 로드하고, 그 전까지 로딩을 지연
+    // VS
+    // 즉시 로딩(Eager Loading) : 연관된 엔티티를 항상 로딩, 성능에 영향을 줄 수 있음, 비효율적일 수 있음
     @ManyToOne(fetch = FetchType.LAZY)
+    // Owner 테이블의 id 컬럼을 참조하여 해당 이름으로 외래 키(Foreign Key) 설정(매핑)
     @JoinColumn(name="ownerId")
+    // JPA에서 관계를 매핑할 때 사용, DB에 직접적으로 반영되지 않음
+    // 아래의 owner 필드를 통해 Owner 엔티티에 접근 가능
     private Owner owner;
 }
--- CardDataBase/src/main/java/org/mind/cardatabase/domain/Owner.java
+++ CardDataBase/src/main/java/org/mind/cardatabase/domain/Owner.java
@@ -26,6 +26,12 @@ public class Owner {
 //    Car를 조회하면서 다시 연결된 OwnerId를 통해
 //    부모를 조회하는 무한 반복에 빠지므로 cars는 json으로 보내지 않는 걸로
     @JsonIgnore
+    //mappedBy 속성
+    //- 양방향 관계에서 연관된 엔티티의 필드 이름을 지정 ex) "owner"
+    //- 양방향 관계에서 연관된 엔티티 간의 관계를 서로 매핑하는 데 사용
+    // CascadeType.ALL
+    // - 부모 엔티티에 적용된 변경 사항이 모든 관련된 자식 엔티티에 전파.
+    // - 부모 엔티티 삭제 시, 자식 엔티티도 삭제
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private List<Car> cars;
 }
