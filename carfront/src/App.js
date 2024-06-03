068ec761f69901b500d5cd7e43725ffecfe7fc92
jidefine <148775766+jidefine@users.noreply.github.com>
2024-06-03 16:17:36 +0900

    DataGrid 사용하여 json 데이터 표시

--- carfront/src/App.js
+++ carfront/src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 // import MainApp from './ex01_main_app/MainApp';
 // import MainApp from './ex02_car_list/MainApp';
-import MainApp from './ex03_server_url/MainApp';
+// import MainApp from './ex03_server_url/MainApp';
+import MainApp from './ex04_data_grid/MainApp';
 
 function App() {
   return <MainApp/>
---
+++ carfront/src/ex04_data_grid/components/constants.js
@@ -0,0 +1,1 @@
+export const SERVER_URL = "htpp://localhost:12000/api/cars";
