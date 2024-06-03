43de06f6aab32a0cb149aca10a96710d7f02ebd4
jidefine <148775766+jidefine@users.noreply.github.com>
2024-06-03 18:42:33 +0900

    삭제 기능 구현

--- carfront/src/App.js
+++ carfront/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 // import MainApp from './ex01_main_app/MainApp';
 // import MainApp from './ex02_car_list/MainApp';
 // import MainApp from './ex03_server_url/MainApp';
-import MainApp from './ex04_data_grid/MainApp';
+// import MainApp from './ex04_data_grid/MainApp';
+import MainApp from './ex05_delete_car/MainApp';
 
 function App() {
   return <MainApp/>
