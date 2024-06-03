eccb0241ac647bc0e27b0703f6c2f009a1c9c678
jidefine <148775766+jidefine@users.noreply.github.com>
2024-06-03 16:07:54 +0900

    서버로부터 car 목록 요청/표시

--- carfront/src/App.js
+++ carfront/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import MainApp from './ex01_main_app/MainApp';
+// import MainApp from './ex01_main_app/MainApp';
+import MainApp from './ex02_car_list/MainApp';
 
 function App() {
   return <MainApp/>
