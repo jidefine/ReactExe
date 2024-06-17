ee1d1cc9ce01fa7f9518f14388ec66423bc8cc43
jidefine <youjio2000@gmail.com>
2024-06-17 23:31:27 +0900

    주석 추가

--- carfront/src/ex12_login/components/Login.jsx
+++ carfront/src/ex12_login/components/Login.jsx
@@ -3,16 +3,19 @@ import { SERVER_URL } from './constants';
 import {Button, TextField, Stack} from '@mui/material';
 
 function Login(){
+    // 사용자 입력과 인증 상태를 관리하는 상태 변수 선언
     cosnt [UserActivation, setUser] = useState({
         username: '',
         password: ''
     });
     const [isAuthenticated, setAuth] = useState(false);
 
+     // 입력 값 변경 시 사용자 상태 업데이트
     const handleChange = (event) => {
         setUser({...user, [event.target.name]: event.target.value});
     }
 
+    // 로그인 처리
     const login = () => {
         fetch(SERVER_URL + 'login', {
             method: 'POST',
@@ -22,6 +25,7 @@ function Login(){
         .then(response => {
             const jwtToken = response.headers.get('Authorization');
             if(jwtToken !== null){
+                // 세션 스토리지에 JWT 토큰 저장하고 인증 상태 업데이트
                 sessionStorage.setItem("jwt", jwtToken);
                 setAuth(true);
             }
@@ -31,6 +35,7 @@ function Login(){
 
     return (
         <div>
+            {/* UI 요소를 수직으로 쌓기 위한 Stack 컴포넌트 */}
             <Stack spacing={2} alignItems='center' mt={2}>
                 <TextField name='username' label='Username' onChange={handleChange} />
                 <TextField type='password' name='password' label='Password' onChange={handleChange} />
