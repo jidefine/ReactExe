51779316ebe19779488c5c2f2493dac5e05d8604
jidefine <youjio2000@gmail.com>
2024-06-23 21:12:08 +0900

    React-Router 추가 및 설정

---
+++ React_learned_by_code/frontend/mall/src/router/root.js
@@ -0,0 +1,14 @@
+// React-Router를 활용해서 마치 기존의 웹페이지처럼
+// 브라우저 주소창에 따라서 다양한 컴포넌트 출력
+
+// 동일한 레이아웃과 동일한 상단 메뉴를 사용
+// 화면마다 필요한 부분만 구현해서 화면에 출력
+
+//root.js는 createBrowserRouter를 통해 어떤 경로에 어떤 컴포넌트를 보여루 줄 것인지 결정
+import { createBrowserRouter } from "react-router-dom";
+
+const root = createBrowserRouter([
+
+])
+
+export default root;
