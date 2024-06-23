423786a69a05c943e68816d40bca94b40473819a
jidefine <youjio2000@gmail.com>
2024-06-23 23:34:41 +0900

    상단 메뉴 컴포넌트 구성

---
+++ React_learned_by_code/frontend/mall/src/components/menus/BasicMenu.js
@@ -0,0 +1,45 @@
+import {Link} from "react-router-dom";
+import logoImage from "../../image/logo.svg"; // 이미지 파일 경로
+
+const BasicMenu = () => {
+    return (
+        <nav id='navbar' className="flex bg-blue-300">
+            <div className="w-4/5 bg-gray-500">
+                <ul className="flex p-3 text-white font-bold">
+                    <li className="pr-6 text-2xl">
+                        {/*로고 삽입*/}
+                        <Link to={'/'}>
+                            <img src={logoImage} alt="Main" className="h-8" />
+                        </Link>
+                    </li>
+                    <li className="pr-6 text-2xl">
+                        <Link to={'/'}>우리는 함께 산다</Link>
+                    </li>
+                    <li className="pr-6 text-2xl">
+                        <Link to={'/about'}>About</Link>
+                    </li>
+                    <li className="pr-6 text-2xl">
+                        <Link to={'/tip/'}>꿀팁</Link>
+                    </li>
+                    <li className="pr-6 text-2xl">
+                        <Link to={'/meeting/'}>모임</Link>
+                    </li>
+                    <li className="pr-6 text-2xl">
+                        <Link to={'/todo/'}>Todo</Link>
+                    </li>
+                    <li className="pr-6 text-2xl">
+                        <Link to={'/mall/'}>mall</Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="w-1/5 justify-end bg-orange-300 p-4 font-medium">
+                <div className="text-white text-sm m-1 rounded" >
+                    Login
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default BasicMenu;
--- React_learned_by_code/frontend/mall/src/layouts/BasicLayout.js
+++ React_learned_by_code/frontend/mall/src/layouts/BasicLayout.js
@@ -1,15 +1,23 @@
+import BasicMenu from "../components/menus/BasicMenu";
+
 // BasicLayout 컴포넌트
 // - 화면 상단에 공통적인 메뉴와 링크를 보여줌
 // - 아래에 각 페이지별 컴포넌트를 출력
 // - children 속성을 활용해서 컴포넌트 내부에 다른 컴포넌트 적용 가능
+
 const BasicLayout = ({children}) => {
     return (
         <>
+        {/* 
         <header className="bg-teal-400 p-2">
             <h1 className="text-2xl md:text-4xl">
                 header
             </h1>
         </header>
+        */}
+
+        {/*헤더 교체*/}
+        <BasicMenu />
 
         <div className="bg-white my-1 w-full flex flex-col space-y-4 md:flex-row md:space-a-4 md:space-y-0">
             <aside className="bg-green-300 md:w-1/5 lg:w-1/4 px-5 py-40">
